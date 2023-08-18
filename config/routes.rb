8d05f02336550a6ad23d470f566e890addeba87b
Ana Sofia Correia <asofiac215@gmail.com>
2023-08-18 12:34:10 +0100

    watch_list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'bookmarks/movies:references'
+  # get 'bookmarks/lists:references'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'lists#index'
+  resources :lists, only: [:index, :new, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
